e2cbe0e33211df0c9302c69f7065a67659f6f952
MMcClure11 <mmcclure0100@gmail.com>
2022-12-15 16:19:36 -0600

    Update versions and paths for new mac

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,6 +2,10 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-source /usr/local/opt/asdf/libexec/asdf.fish
+# source /usr/local/opt/asdf/libexec/asdf.fish
 set -gx STOW_DIR ~/.dotfiles
 set -gx ERL_FLAGS "-kernel shell_history enabled"
+source /opt/homebrew/opt/asdf/libexec/asdf.fish
+
+source /opt/homebrew/opt/asdf/libexec/asdf.fish
+direnv hook fish | source
