c0eba7c13c68669ab0aff375d2cb167068abbbec
MMcClure11 <mmcclure0100@gmail.com>
2023-02-21 19:01:17 -0600

    Add TailwindSort plugin

--- nvim/.config/nvim/lua/config/plugins/init.lua
+++ nvim/.config/nvim/lua/config/plugins/init.lua
@@ -2,8 +2,19 @@ return {
   { "voldikss/vim-floaterm", keys = {
     { "<Leader>g", "<cmd>FloatermNew --width=0.9 --height=0.9 lazygit<cr>", desc = "Open lazygit in Floaterm" }
   } },
+
   -- Editing
   "tpope/vim-surround",
+  {
+    'laytan/tailwind-sorter.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-lua/plenary.nvim' },
+    build = 'cd formatter && npm i && npm run build',
+    config = {
+      on_save_enabled = true,
+      on_save_pattern = { "*.heex", "*.ex" }
+    },
+  },
+
   -- Navigation
   "andymass/vim-matchup",
   "justinmk/vim-dirvish",
