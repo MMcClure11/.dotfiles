63ae9fe386b54a003e198eae073fcc6162a33b1f
MMcClure11 <mmcclure0100@gmail.com>
2023-06-29 17:58:10 -0600

    Update config.fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,11 +1,15 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+# Commands to run in interactive sessions can go here
+  function fish_user_key_bindings
+    fish_vi_key_bindings
+  end
 end
 
 # source /usr/local/opt/asdf/libexec/asdf.fish
 set -gx STOW_DIR ~/.dotfiles
 set -gx ERL_FLAGS "-kernel shell_history enabled"
 set -Ux ERL_AFLAGS "-kernel shell_history enabled"
+set -Ux EDITOR nvim
 source /opt/homebrew/opt/asdf/libexec/asdf.fish
 
 source /opt/homebrew/opt/asdf/libexec/asdf.fish
