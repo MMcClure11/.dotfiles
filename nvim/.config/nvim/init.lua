f999ca0ae7068fe6cac24924cc0f4c37b7f8cc4c
MMcClure11 <mmcclure0100@gmail.com>
2023-08-02 11:15:23 -0600

    Remove handbuilt test runner, want to reuse the keybindings

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -32,12 +32,3 @@ require("lazy").setup("config.plugins", {
     colorscheme = { "tokyonight-night" },
   },
 })
-
--- This is a lua file, have to tell it that we are running vimscript
-local quotes = { 'Nick is da best mentor.', 'Yury is hilarious, cares a lot, and has got your back.',
-  'Jordan says, You can do hard things.', 'You are enough',
-  'Be the weirdest, strangest, most unhinged part of someone’s day.' }
-local quote = quotes[math.random(#quotes)]
-vim.g.mcm_test_line = quote
-vim.cmd([[nnoremap <leader>tn <cmd>exe "let g:mcm_test_line = expand('%') . '\:' . line('.') <bar> !mix test " . mcm_test_line <cr>]])
-vim.cmd([[nnoremap <leader>tt <cmd>exe "!mix test " . mcm_test_line <cr>]])
