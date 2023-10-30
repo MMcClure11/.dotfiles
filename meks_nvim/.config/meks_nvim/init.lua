3d73e6ea391cf7990e7843c19300802396b8a05a
MMcClure11 <mmcclure0100@gmail.com>
2023-10-30 19:48:15 -0300

    Turn off cursorline

---
+++ meks_nvim/.config/meks_nvim/init.lua
@@ -0,0 +1,6 @@
+-- opt is a “map” with a key cursorline
+-- you can “inspect” via the command line `:= vim.opt`
+-- when inspecting, it's helpful to get the value of the opt: `:= vim.opt.cursorline:get()`
+-- also `:help cursorline`
+vim.opt.cursorline = false
+
