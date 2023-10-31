0dfdd34b7426cc1286d1ebe1951d3e0c158b38a4
MMcClure11 <mmcclure0100@gmail.com>
2023-10-31 13:46:37 -0300

    Update mini.lua so comments work again (gcc)

--- nvim/.config/nvim/lua/config/plugins/mini.lua
+++ nvim/.config/nvim/lua/config/plugins/mini.lua
@@ -1,21 +1,8 @@
-local M = { "echasnovski/mini.nvim" }
-
-function M.align()
-  require("mini.align").setup()
-end
-
-function M.comment()
-  require("mini.comment").setup({})
-end
-
-function M.pairs()
-  require("mini.pairs").setup({})
-end
-
-function M.config()
-  M.align()
-  M.comment()
-  M.pairs()
-end
-
-return M
+return {
+  "echasnovski/mini.nvim",
+  config = function()
+    require("mini.align").setup()
+    require("mini.comment").setup({})
+    require("mini.pairs").setup({})
+  end
+}
