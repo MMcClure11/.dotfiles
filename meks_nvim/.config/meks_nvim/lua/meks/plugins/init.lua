ff7bc28e8204bffcadb085a1132c53e44f9f33e6
MMcClure11 <mmcclure0100@gmail.com>
2023-11-08 23:28:36 -0300

    Add plugins directory and init.lua

---
+++ meks_nvim/.config/meks_nvim/lua/meks/plugins/init.lua
@@ -0,0 +1,21 @@
+return {
+  -- this could be blank if I decide to move these to a “colors” file
+  -- even if empty, this is a good place to test plugins before committing to
+  -- adding them to the config
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    config = function()
+      -- double [[]] is another way to make a string
+      -- vim.cmd("colorscheme tokyonight")
+      vim.cmd([[colorscheme tokyonight-night]])
+    end
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000
+  }
+}
