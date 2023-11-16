cfe2be74941739fd322297bb3b48a58f300b01d6
MMcClure11 <mmcclure0100@gmail.com>
2023-11-15 21:18:14 -0300

    Add lsp keybindings
    Requires knowledge of Autocommands and Autogroups

--- meks_nvim/.config/meks_nvim/lua/meks/plugins/lsp.lua
+++ meks_nvim/.config/meks_nvim/lua/meks/plugins/lsp.lua
@@ -1,3 +1,9 @@
+-- to run anything outside a return function, it has to become before it otherwise lua will be hella mad
+-- print you can see with `:messages` (because it was not the last message)
+-- vim.api.nvim_create_autocmd("BufWritePre",
+--  { group = vim.api.nvim_create_augroup('tommy', {}), callback = function() print(
+--   "I want strawberries and peanut butter.") end })
+
 return {
   -- for our lsp setup, we can specify and make it have a dependency (such as mason)
   -- we have our own personal dependencies, lsp will work without them, but my lsp config needs them to work
@@ -27,5 +33,31 @@ return {
     lspconfig.tsserver.setup {}
     lspconfig.sqlls.setup {}
     lspconfig.lua_ls.setup {}
+
+    -- KEYBINDINGS
+    -- we don’t want to activate the keybinding unless the language server is attached to the buffer
+    -- keys option that Lazy.nvim option gives us, the plugin won’t load until we press the key combos
+    -- but here, we want the lsps to load as soon as a buffer opens that it can attach to
+    -- AUTOCOMMANDS
+    -- general vim concept
+    -- you can attach behaviors to events
+    -- good idea to put auto cmds inside autocmd groups so they don’t get duplicated
+    vim.api.nvim_create_autocmd("LspAttach",
+      {
+        group = vim.api.nvim_create_augroup('tommy', {}),
+        callback = function(event)
+          -- IO.inspect converts map into a string that can be printed. vim.print does the same type of thing
+          -- we have a buf key in the event table
+          -- vim.print(event)
+          vim.keymap.set("n", "<localleader>s", "<cmd>Telescope lsp_document_symbols<cr>",
+            { desc = "list document symbols (things like function heads, test names, etc.", buffer = event.buf }
+          )
+
+          -- this should be working, it maybe doesn’t work for short diagnostic strings. Come back to it, check if working with bigger errors
+          vim.keymap.set("n", "<localleader>d", vim.diagnostic.open_float,
+            { desc = "open float with diagnostic description", buffer = event.buf }
+          )
+        end
+      })
   end
 }
