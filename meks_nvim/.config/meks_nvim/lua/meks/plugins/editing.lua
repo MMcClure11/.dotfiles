a09e0c762972c40ce4e37a03218be5d2c75673ea
MMcClure11 <mmcclure0100@gmail.com>
2024-06-13 17:37:44 +0100

    Add leap

--- meks_nvim/.config/meks_nvim/lua/meks/plugins/editing.lua
+++ meks_nvim/.config/meks_nvim/lua/meks/plugins/editing.lua
@@ -3,4 +3,13 @@ return {
   -- adds `gc` and `gcc`
   { "echasnovski/mini.comment",    version = false, config = true },
   { "echasnovski/mini.statusline", version = false, config = true },
+  -- leap
+  -- :map s
+  -- :map <s-s> (how to check shift key)
+  {
+    "ggandor/leap.nvim",
+    config = function()
+      require('leap').create_default_mappings()
+    end
+  },
 }
