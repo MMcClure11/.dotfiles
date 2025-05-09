4089084cddc82ee57b228f0dbd94977ede2f3f7a
MMcClure11 <mmcclure0100@gmail.com>
2025-05-09 19:07:13 +0200

    Update mason/lsp to work with nvim version 11

--- nvim/.config/nvim/lua/config/plugins/lsp/init.lua
+++ nvim/.config/nvim/lua/config/plugins/lsp/init.lua
@@ -21,15 +21,6 @@ function M.config()
   require("neodev").setup({})
   require("mason").setup()
 
-  local function on_attach(client, buffer)
-    require("config.plugins.lsp.keymaps").setup(client, buffer)
-    -- require("config.plugins.lsp.formatting").setup(client, buffer)
-
-    vim.opt_local.foldmethod = "expr"
-    vim.opt_local.foldexpr = "nvim_treesitter#foldexpr()"
-    vim.opt.foldlevel = 99
-  end
-
   local capabilities = vim.lsp.protocol.make_client_capabilities()
   capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
@@ -38,16 +29,6 @@ function M.config()
   mason_lspconfig.setup({
     ensure_installed = vim.tbl_keys(servers),
   })
-
-  mason_lspconfig.setup_handlers {
-    function(server)
-      require("lspconfig")[server].setup {
-        capabilities = capabilities,
-        on_attach = on_attach,
-        settings = servers[server],
-      }
-    end,
-  }
 end
 
 return M
