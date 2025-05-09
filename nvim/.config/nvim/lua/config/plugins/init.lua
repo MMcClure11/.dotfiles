0294cd49c2389141fd6a617ea5e82b1a759a43f2
MMcClure11 <mmcclure0100@gmail.com>
2025-05-09 14:26:26 +0200

    Add Fidget, Dadbod UI, and avante

--- nvim/.config/nvim/lua/config/plugins/init.lua
+++ nvim/.config/nvim/lua/config/plugins/init.lua
@@ -61,5 +61,54 @@ return {
   {
     "stevearc/conform.nvim",
     opts = { format_on_save = { lsp_format = "fallback" } }
-  }
+  },
+
+  -- Fidget
+  { "j-hui/fidget.nvim", opts = {} },
+
+  {
+    'kristijanhusak/vim-dadbod-ui',
+    dependencies = {
+      { 'tpope/vim-dadbod',                     lazy = true },
+      { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true }, -- Optional
+    },
+    cmd = {
+      'DBUI',
+      'DBUIToggle',
+      'DBUIAddConnection',
+      'DBUIFindBuffer',
+    },
+    init = function()
+      -- Your DBUI configuration
+      vim.g.db_ui_use_nerd_fonts = 1
+    end,
+  },
+
+  -- Trying out AI copilot
+  { "github/copilot.vim" }
+  -- {
+  --   "yetone/avante.nvim",
+  --   event = "VeryLazy",
+  --   lazy = false,
+  --   version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
+  --   opts = {},
+  --   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+  --   -- build is a special key for running a shell command after it runs the plugine command
+  --   build = "make",
+  --   -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+  --   dependencies = {
+  --     "nvim-treesitter/nvim-treesitter",
+  --     "stevearc/dressing.nvim",
+  --     "nvim-lua/plenary.nvim",
+  --     "MunifTanjim/nui.nvim",
+  --     {
+  --       -- Make sure to set this up properly if you have lazy=true
+  --       'MeanderingProgrammer/render-markdown.nvim',
+  --       opts = {
+  --         file_types = { "markdown", "Avante" },
+  --       },
+  --       ft = { "markdown", "Avante" },
+  --     },
+  --   },
+  -- }
 }
