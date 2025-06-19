3349192ba375ca0a5cd81d1b571426f09ff7aa78
MMcClure11 <mmcclure0100@gmail.com>
2025-06-19 11:42:59 -0500

    feat(nvim): add snacks.nvim plugin with indent guides
    - Add snacks.nvim meta plugin with priority 1000
    - Configure indent guide with Statement highlight color
    - Update lazy-lock.json with new plugin dependency

--- .config/nvim/lua/config/plugins/init.lua
+++ .config/nvim/lua/config/plugins/init.lua
@@ -134,7 +134,25 @@ return {
       require("mcphub").setup()
     end
   },
+  -- meta plugin for moar plugins
   {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    ---@type snacks.Config
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+      indent = {
+        enabled = true,
+        scope = {
+          -- change the line highlight color
+          -- "<cmd>hi" to see list of available colors
+          hl = "Statement"
+        }
+      },
+    },
   }
 
   -- Trying out AI copilot
